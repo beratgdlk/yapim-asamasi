1a4eb5548f80295df2e5ea8cbabf4c085aeddea6
Berat Güdelek <beratgdlk@gmail.com>
2024-11-03 21:24:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loadingIcon = document.getElementById('loading-icon');
+    loadingIcon.innerHTML = '<div class="spinner"></div>';
+});
